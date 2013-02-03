e60fb1cbaec2129d16ef1239846ef3949fc566ea
Kan Fushihara <kan.fushihara@gmail.com>
2013-02-03 09:31:32 +0900

    本編公開まで情報制限

--- lib/Acme/PrettyCure/Girl/CureSword.pm
+++ lib/Acme/PrettyCure/Girl/CureSword.pm
@@ -4,10 +4,9 @@ use Moo;
 
 with qw/Acme::PrettyCure::Girl::Role Acme::PrettyCure::Girl::Role::DokiDoki/;
 
-sub human_name   {'剣崎真琴'}
+sub human_name   {'???'}
 sub precure_name {'キュアソード'}
-sub age          {14}
-sub challenge { '勇気の刃!キュアソード' }
+sub challenge { '' }
 sub color { 183 }
 sub image_url { 'http://www.toei-anim.co.jp/tv/dd_precure/img/character/chara_img02_04.png' }
 
